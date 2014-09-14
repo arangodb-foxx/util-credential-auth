246d44a1ed41a03753f74dd2258c27f369b90e3b
Alan Plum <me@pluma.io>
2014-09-14 13:43:38 +0200

    Only support HMAC-SHA1. Bump to 1.0.0.

--- auth.js
+++ auth.js
@@ -9,45 +9,46 @@
     if (!authData) {
       authData = {};
     }
-    var hashMethod = authData.hashMethod || 'pbkdf2-hmac-' + (cfg.hashMethod || 'sha1'),
-      workUnits = authData.workUnits || cfg.workUnits || 60,
-      workKey = cfg.workKey || 388;
-
-    var match = hashMethod.match(/^pbkdf2-hmac-(\w+)$/)
-    if (match) {
-      hashmethod = match[1];
-    } else if (hashMethod === 'pbkdf2') {
-      hashMethod = 'sha1';
+    if (typeof authData === 'string') {
+      authData = JSON.parse(authData);
     }
+    if (authData.hashMethod && authData.hashMethod !== 'pbkdf2') {
+      throw new Error('Unsupported hash method: ' + authData.hashMethod);
+    }
+
+    var workUnits = authData.workUnits || cfg.workUnits || 60,
+      workKey = cfg.workKey || 388;
 
     // non-lazy comparison to avoid timing attacks
     return crypto.constantEquals(
-      authData.hash || '', new Buffer(crypto.pbkdf2hmac(
+      authData.hash || '', new Buffer(crypto.pbkdf2(
       authData.salt || '',
       password,
       (baseline + workKey) * workUnits,
-      authData.keyLength || cfg.keyLength || 66,
-      hashMethod
+      authData.keyLength || cfg.keyLength || 66
     ), 'hex').toString('base64'));
   }
 
   function hashPassword(password) {
-    var hashMethod = cfg.hashMethod,
-      salt = crypto.genRandomAlphaNumbers(cfg.keyLength || 66),
+    var salt = crypto.genRandomAlphaNumbers(cfg.keyLength || 66),
       keyLength = cfg.keyLength || 66,
       workUnits = cfg.workUnits || 60,
       baseline = cfg.baseline || 1000,
       workKey = cfg.workKey || 388,
       iterations = (baseline + workKey) * workUnits,
-    return {
-      hash: new Buffer(crypto.pbkdf2hmac(
-        salt, password, iterations, keyLength, hashMethod
+      data;
+
+    data = {
+      hash: new Buffer(crypto.pbkdf2(
+        salt, password, iterations, keyLength
       ), 'hex').toString('base64'),
       salt: salt,
       keyLength: keyLength,
-      hashMethod: (hashMethod === 'sha1') ? 'pbkdf2' : 'pbkdf2-hmac-' + hashMethod,
+      hashMethod: 'pbkdf2',
       workUnits: workUnits
     };
+
+    return cfg.json ? JSON.stringify(data) : data;
   }
 
   exports.verifyPassword = verifyPassword;
