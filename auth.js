e37966651d3850805a9e02f39b86a3092a56864c
Alan Plum <me@pluma.io>
2015-02-25 13:10:59 +0100

    Cleanup for 2.5.

--- auth.js
+++ auth.js
@@ -1,56 +1,52 @@
-/*global require, exports, applicationContext */
-(function () {
-  'use strict';
-  var crypto = require('org/arangodb/crypto'),
-    cfg = applicationContext.configuration,
-    baseline = 1000;
+/*global require, exports, applicationContext, Buffer */
+'use strict';
+var crypto = require('org/arangodb/crypto');
+var cfg = applicationContext.configuration;
+var baseline = 1000;
 
-  function verifyPassword(authData, password) {
-    if (!authData) {
-      authData = {};
-    }
-    if (typeof authData === 'string') {
-      authData = JSON.parse(authData);
-    }
-    if (authData.hashMethod && authData.hashMethod !== 'pbkdf2') {
-      throw new Error('Unsupported hash method: ' + authData.hashMethod);
-    }
-
-    var workUnits = authData.workUnits || cfg.workUnits || 60,
-      workKey = cfg.workKey || 388;
-
-    // non-lazy comparison to avoid timing attacks
-    return crypto.constantEquals(
-      authData.hash || '', new Buffer(crypto.pbkdf2(
-      authData.salt || '',
-      password || '',
-      (baseline + workKey) * workUnits,
-      authData.keyLength || cfg.keyLength || 66
-    ), 'hex').toString('base64'));
+function verifyPassword(authData, password) {
+  if (!authData) {
+    authData = {};
+  }
+  if (typeof authData === 'string') {
+    authData = JSON.parse(authData);
+  }
+  if (authData.hashMethod && authData.hashMethod !== 'pbkdf2') {
+    throw new Error('Unsupported hash method: ' + authData.hashMethod);
   }
 
-  function hashPassword(password) {
-    var salt = crypto.genRandomAlphaNumbers(cfg.keyLength || 66),
-      baseline = 1000,
-      keyLength = cfg.keyLength || 66,
-      workUnits = cfg.workUnits || 60,
-      workKey = cfg.workKey || 388,
-      iterations = (baseline + workKey) * workUnits,
-      data;
+  var workUnits = authData.workUnits || cfg.workUnits || 60;
+  var workKey = cfg.workKey || 388;
 
-    data = {
-      hash: new Buffer(crypto.pbkdf2(
-        salt, password, iterations, keyLength
-      ), 'hex').toString('base64'),
-      salt: salt,
-      keyLength: keyLength,
-      hashMethod: 'pbkdf2',
-      workUnits: workUnits
-    };
+  // non-lazy comparison to avoid timing attacks
+  return crypto.constantEquals(
+    authData.hash || '', new Buffer(crypto.pbkdf2(
+    authData.salt || '',
+    password || '',
+    (baseline + workKey) * workUnits,
+    authData.keyLength || cfg.keyLength || 66
+  ), 'hex').toString('base64'));
+}
 
-    return cfg.json ? JSON.stringify(data) : data;
-  }
+function hashPassword(password) {
+  var salt = crypto.genRandomAlphaNumbers(cfg.keyLength || 66);
+  var baseline = 1000;
+  var keyLength = cfg.keyLength || 66;
+  var workUnits = cfg.workUnits || 60;
+  var workKey = cfg.workKey || 388;
+  var iterations = (baseline + workKey) * workUnits;
+  var data = {
+    hash: new Buffer(crypto.pbkdf2(
+      salt, password, iterations, keyLength
+    ), 'hex').toString('base64'),
+    salt: salt,
+    keyLength: keyLength,
+    hashMethod: 'pbkdf2',
+    workUnits: workUnits
+  };
 
-  exports.verifyPassword = verifyPassword;
-  exports.hashPassword = hashPassword;
-}());
+  return cfg.json ? JSON.stringify(data) : data;
+}
+
+exports.verifyPassword = verifyPassword;
+exports.hashPassword = hashPassword;
